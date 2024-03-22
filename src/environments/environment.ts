f234da6b7fee6f03750d60a434c0e7d14841baf7
CBLuis <carlos.carpiob.est@uets.edu.ec>
2024-03-22 16:05:59 -0500

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,firebase: {
+    apiKey: 'AIzaSyCbpEN_BGy9i1KlI-Yr7_jnYVKwP7BQTkc',
+    authDomain: 'domotica-carpiogaibor.firebaseapp.com',
+    projectId: 'domotica-carpiogaibor',
+    storageBucket: 'domotica-carpiogaibor.appspot.com',
+    messagingSenderId: '926782459401',
+    appId: '1:926782459401:web:1bf835ed0c3cec1c16b2d8',
+  }
 };
 
 /*
